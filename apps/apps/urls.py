0be51b5cd925936fd6d14f8174340cc7a0192f98
Chidera Divine-gift <chrischidera6@gmail.com>
2026-05-21 10:32:12 +0100

    fix(urls): correct import path from "v1/imports/" to "v1/import/"

--- apps/apps/urls.py
+++ apps/apps/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("v1/audit/", include("vs_audit.urls")),
     path('v1/config/', include('vs_config.urls')),
     path('v1/notify/', include('vs_notifications.urls')),
-    path("v1/imports/", include("vs_import_data.urls")),
+    path("v1/import/", include("vs_import_data.urls")),
     # path("admin/", admin.site.urls),
     # Authentication
     # path("v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
