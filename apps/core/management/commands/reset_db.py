cfee8625277e84ea2b1f1fe742c848854e9e9d5f
Chidera Divine-gift <chrischidera6@gmail.com>
2026-06-07 13:39:01 +0100

    feat(reset_db): update default post-commands to include 'seed_all_permissions'

--- apps/core/management/commands/reset_db.py
+++ apps/core/management/commands/reset_db.py
@@ -68,7 +68,7 @@ class Command(BaseCommand):
             '--post-commands',
             nargs='+',
             type=str,
-            default=["seed_actions", "seed_prebuilt_role_templates", "create_superuser", "seed_package", "seed_xvs_modules"],
+            default=["seed_actions", "seed_all_permissions", "create_superuser", "seed_package", "seed_xvs_modules"],
             help='Commands to run after migration completes (e.g., seed_roles seed_schools)'
         )
 
