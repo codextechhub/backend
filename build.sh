c262a93ce92eb90e91ae1be77ec954943a78811a
Chidera Divine-gift <chrischidera6@gmail.com>
2026-06-12 17:12:04 +0100

    fix: remove seeding command for permissions from build script

--- build.sh
+++ build.sh
@@ -11,6 +11,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Run seeding commands AFTER migrate succeeds
-python manage.py seed_all_permissions
 # python manage.py reset_db --yes
 # python manage.py create_superuser --assign-role --email admin@codexng.com
